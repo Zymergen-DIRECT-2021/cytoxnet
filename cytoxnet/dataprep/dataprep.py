c114dc85f7259c79cf56bbf8cdb69cd7dc04cb02
Rory McGuire <roryjmcguire@gmail.com>
2021-04-22 14:21:53 -0700

    Updated pseudocode to match compspec

--- cytoxnet/dataprep/dataprep.py
+++ cytoxnet/dataprep/dataprep.py
@@ -1,19 +1,14 @@
-## potentially useful: deepchem dataset class or pandas split
-## having our data in the deepchem.dataset form at the end is very helpful
-
-
 """
 Data splitting module for creating train/test splits  
 """
 
 #import pandas as pd
 #import deepchem
-#import sklearn
 
 
-#def convert_cats(dataframe):
+#def convert_to_categorical(dataframe, cols: list of str):
     """
-    Drops rows with missing values and converts categorical values to integers.
+    Converts non-numerical categorical values to integers.
     
     Parameters
     ----------
@@ -21,93 +16,56 @@ Data splitting module for creating train/test splits
     
     Returns
     -------
-    modified dataframe without any na values in the 'Mol' column 
-    and all categorical columns with string values converted to integer values
+    modified dataframe with user selected columns with categorical string values converted to integer values
     """
-    
-    ## Drop all na values in the 'Mol' column
-    ## May want to expand the range of columns beyond 'Mol'
-    
-    #dataframe = dataframe.dropna()
 
-    ## convert categorical values, if any exist, into integers
-    ## might be able to use sklearn Label Encoder or OnHotEncoder
+    ## convert categorical string values within selected columns into integers
+    ## might be able to use sklearn Label Encoder, OnHotEncoder, or pandas dummies values
     ## might be necessary if we categorize target areas/tox methods/etc.
-    ## Iterate through columns and convert any containing strings to ints
-    
-    #for column in dataframe:
-    #  column_series_obj = dataframe[column]
-    #	if dataframe[column].dtype not numeric:
-    #    l = sklearn.preprocessing.LabelEncoder()
-    #		l.fit(dataframe[column])
-    #		dataframe.column = l.transform(dataframe.column)
-    #	else continue
+
+    ## generate binary values using get_dummies
+    #dataframe = pd.get_dummies(dataframe, columns=cols)    
+
     #return dataframe
 
 
+#def convert_to_dataset(dataframe, X_col: str = 'X', y_col: str = 'y', w_col: str = None, id_col: str = None):
+	"""
+	Converts dataframe into a deepchem dataset object.
 
+	Parameters
+	----------
+	dataframe: featurized dataframe
+	X_col: (str or List[str]) name(s) of the column(s) containing the X array.
+   	y_col: (str or List[str]) name(s) of the column(s) containing the y array.
+   	w_col: (str or List[str]) name(s) of the column(s) containing the w array.
+   	id_col: (str) name of the column containing the ids.
 
+   	Returns
+   	-------
+   	deepchem dataset
+	"""
 
+	#to_dataset = dc.data.NumpyDataset(X, y, w, ids)
+    #dataset = to_dataset.from_dataframe(dataframe)
 
-#def trans_data(dataframe, X, y, w, ids, 
-#	transform_list: list = ['Normalize',], to_transform: str = 'X', **kwargs):
-    
-    """
-    Converts featurized dataframe into a transformed dataset object.
-    
-    Parameters
-    ----------
-   	dataframe: featurized dataframe 
-   	X: (str or List[str]) name(s) of the column(s) containing the X array.
-   	y: (str or List[str]) name(s) of the column(s) containing the y array.
-   	w: (str or List[str]) name(s) of the column(s) containing the w array.
-   	ids: (str) name of the column containing the ids.
-   	transform_list: (List[str]) list of transformation methods to pass dataset through
+    #return dataset
+
+#def preprocess(
+#	dataset, 
+#	transformations: list of str = ['NormTransform',], to_transform: str = 'X', 
+#	splitter: str = 'RandomSplitter', split_type: 'train_valid_test_split', k = None, frac_train = None, frac_valid = None, frac_test = None, seed = None, 
+#	**kwargs
+#	):
+
+	"""
+	Transforms and splits deepchem dataset
+
+	Parameters
+	----------
+	dataset: deepchem dataset to be transformed and/or split
+	transformations: (List[str]) list of transformation methods to pass dataset through
    	to_transform: (str) specification of whether to transform X, y, or w
-   	**kwargs: key word arguments to be passed to selected transformer
-
-
-    Returns
-    -------
-    transformed dataset object
-    """
-    
-    ## convert dataframe to dataset using deepchem.data.NumpyDataset function
-    
-    #to_dataset = deepchem.data.NumpyDataset(X, y, w, ids)
-	#dataset = to_dataset.from_dataframe(dataframe)
-
-    #if to_transform == 'X':
-    #	transform_X = True
-    #    transform_y = False
-   	#elif to_transform == 'y':
-    #    transform_X = False
-    #    transform_y = True
-
-    ## feed dataset into list of transformers sequentially, returning a single transformed dataset
-   	
-    #for transform in transform_list:
-    #   	transformer = getattr(deepchem.transformers, transform)(transform_X=transform_X, transform_y=transform_y, dataset=dataset **kwargs)
-    #   	dataset = transformer.transform(dataset)
-
-    #return dataset    
-
-
-
-
-
-
-
-
-#def split_data(dataset, method='RandomSplitter', split_type='train_valid_test_split', 
-#	k=None, frac_train=None, frac_valid=None, frac_test=None, seed=None, **kwargs):
-
-    """
-    Converts processed dataset into split dataset objects.
-    
-    Parameters
-    ----------
-   	dataset: dataset object to be split
    	method: (str) class of deepchem split method
    	split_type: (str) type of split (k_fold_split/train_test_split/train_valid_test_split)
    	k: (int) number of folds to split dataset into
@@ -115,16 +73,28 @@ Data splitting module for creating train/test splits
    	frac_valid: (float) fraction of data to be used for validation split
    	frac_test: (float) fraction of data to be used for test split
    	seed: (int) random seed to use
-   	**kwargs: keyward arguments specific to the input method
+   	**kwargs: keyword arguments to be passed to the selected transformer and splitter
 
-    
-    Returns
-    -------
-    tuple or list of dataset objects
-    """
-    
-    ## convert dataframe to dataset using deepchem.data.NumpyDataset function
-    
+   	Returns
+   	-------
+   	Transformed and/or split ML ready dataset
+	"""
+
+	#if to_transform == 'X':
+	#	transform_X = True
+	#	transform_y = False
+	#elif to_transform == 'y':
+	#	transform_X = False
+	#	transform_y = True
+
+    ## feed dataset into list of transformers sequentially, returning a single transformed dataset
+   	
+    #for transformation in transformations:
+    #	transformer = getattr(deepchem.transformers, transformation)(transform_X=transform_X, transform_y=transform_y, dataset=dataset **kwargs)
+    #	dataset = transformer.transform(dataset)
+
+    ## split data
+
     #data_splitter = getattr(deepchem.splits, method)(**kwargs)
 
     ## this only allows the following three split_types to be used
@@ -165,6 +135,47 @@ Data splitting module for creating train/test splits
     #else: 
     #    continue
 
-    #return data_split
+    #return split_data
+
+
+#def pipeline(datafile: str, id_cols: list of str, descriptor_cols: list of str, target_cols: list of str, **kwargs):
+	"""
+	Wrapping function for dataprep py files:
+	- io.py
+	- featurize.py
+	- dataprep.py
+	# note: may want to include analyze.py somehow
+
+	Parameters
+	----------
+	datafrile: (str) datafile location to be converted to dataframe, then deepchem dataset.
+	id_cols: (str or List[str]) columns to set as id column and check for repeats.
+	descriptor_cols: (str or List[str]) columns with descriptor values.
+	target_cols: (str or List[str]) columns with target values.
+	**kwargs: keyword arguments for wrapped functions.
+	# note: will likely need to be added to/updated as wrapped functions are written.
+
+	Returns
+	-------
+	Dataset(s) preprocessed and ready for ml.
+	"""
+
+	#cols = [id_cols, descriptor_cols, target_cols,]
+	
+	#dataframe = load_file(datafile, cols)
+	#load_XXX() # will need to see how this is written to know where it fits in
+	#dataframe = clean_dataframe(dataframe, cols, duplicates = drop, nans = drop)
+	#dataframe = featurize(dataframe, target_cols, id_col,)
+	#dataframe = convert_to_categorical(dataframe, cols) # this 'cols' value should not be confused with the above 'cols' list
+	#dataset = convert_to_dataset(dataframe, ) # additional arguments will have to be added to the wrapping function
+	#ml_ready_dataset = preprocess(dataset, ) # additional arguments will have to be added to the wrapping function
+
+	#return ml_ready_dataset
+
+
+
+
+
+
 
 

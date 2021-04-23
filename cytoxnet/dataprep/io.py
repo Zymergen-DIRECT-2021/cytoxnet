15f0642db2530b79f802287c52d58dbaebe9bc3b
Nida <nidajanu@uw.edu>
2021-04-22 17:32:36 -0700

    added code for loading and featurizing data

--- cytoxnet/dataprep/io.py
+++ cytoxnet/dataprep/io.py
@@ -3,27 +3,36 @@ Input/Output module for loading data and generating output files.
 """
 
 import pandas as pd
+import os
 
 
-def load_data(datafile, columns='all'):
+def load_data(csv_file, col_id = None):
     """
     Loads a data file into a dataframe containing the raw data.
 
     Parameters
     ----------
     datafile: the file containing the data to be loaded
-    columns: the column (names?numbers?either?) that the user wants to load,
-        default='all'
+    col_id: the column (names?numbers?either?) that the user wants to 
+        use to remove duplicates (ie remove any duplicates based on 
+        the inputted column id), default=None
 
     Returns
     -------
-    DataFrame containg the raw data from the csv
+    DataFrame containg the raw data from the csv,
+    with duplicates removed if a column for doing so
+    is passed
 
     """
     # assert file exists and contains data
-    # remove/ignore unwanted columns
+    assert os.path.exists(csv_file), 'File name does not exist'
     # run any more checks specific to the data that we may want to add
-    # support for multiple file types?
     # load a csv file into a dataframe
-    df = pd.load_csv(datafile)
-    return df
+    df = pd.read_csv(csv_file)
+    if col_id != None:
+        df_1 = df.drop_duplicates(subset=col_id)
+    else:
+        df_1 = df
+    df_2 = df_1.reset_index()
+    # remove/ignore unwanted columns? 
+    return df_2

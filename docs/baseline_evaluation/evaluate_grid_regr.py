f63e6029c468aa02388b6a1e5f6f32b2431e7074
Evan Komp <evankomp@uw.edu>
2021-05-27 14:37:26 -0700

    grids for regression and classification done

---
+++ docs/baseline_evaluation/evaluate_grid_regr.py
@@ -0,0 +1,35 @@
+import cytoxnet.models.evaluate as ev
+
+targets_codex = {
+    'lunghini_fish_LC50': 'fish_LC50',
+    'lunghini_daphnia_EC50': 'daphnia_EC50',
+    'lunghini_algea_EC50': 'algea_EC50',
+    'zhu_rat_LD50': 'rat_LD50',
+    'chembl_ecoli_MIC': 'ecoli_MIC'
+}
+datafiles = [
+    'lunghini_fish_LC50',
+    'lunghini_daphnia_EC50',
+    'lunghini_algea_EC50',
+    'zhu_rat_LD50',
+    'chembl_ecoli_MIC'
+]
+ml_models = ['RFR', 'GPR', 'LASSO']
+featurizers = [
+    'MordredDescriptors',
+    'CircularFingerprint',
+    'RDKitDescriptors',
+    'MACCSKeysFingerprint'
+]
+
+
+df = ev.grid_evaluate_crossval(
+    datafiles,
+    ml_models,
+    featurizers,
+    targets_codex,
+    parallel=True,
+    transformations=['NormalizationTransformer'],
+    codex='database/compounds.csv'
+)
+df.to_csv('regression_grid_results.csv')

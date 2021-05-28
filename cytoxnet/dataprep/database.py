35a7112a8f767576c780d69556fb6ab6d5bc433f
Rory McGuire <roryjmcguire@gmail.com>
2021-05-28 14:53:37 -0700

    Revised the querying function in database.py to take in only two tables at a time and return a full-outer join when two tables are passed

--- cytoxnet/dataprep/database.py
+++ cytoxnet/dataprep/database.py
@@ -8,52 +8,57 @@ def table_creator(table_name, dataframe):
     - table_name: str of name assigned to table.
     - dataframe: dataframe object
     """
-    
-    # dictionary to convert  between pandas dtypes and sqlalchemy dtypes    
+
+    # dictionary to convert  between pandas dtypes and sqlalchemy dtypes
     data_types = {
         'object': String,
         'int64': Integer,
         'float64': Float,
         'bool': Boolean,
-        'datetime64':DateTime,
-        'timedelta[ns]':Interval,
-        'category':String
+        'datetime64': DateTime,
+        'timedelta[ns]': Interval,
+        'category': String
     }
-    
+
     # create list of column names and associated data types
     column_names = []
     column_types = []
     for column in dataframe.columns:
         column_names.append(column)
         column_types.append(dataframe[column].dtypes.name)
-    
+
     print(column_types)
-        
+
     # remove 'id' and 'smiles' columns from dataframe
     # this will be important when creating the database table
     del column_names[0:2]
     del column_types[0:2]
-    
+
     # create list of sqlalchamy data types
     column_sqlalchemy_types = []
     for column_type in column_types:
         column_sqlalchemy_types.append(data_types.get(column_type))
-        
-    # create new dictionary with column names as keys and sqlalchemy data types a values
+
+    # create new dictionary with column names as keys and sqlalchemy data
+    # types a values
     header_info = dict(zip(column_names, column_sqlalchemy_types))
-    #print(header_info)
-    
+    # print(header_info)
+
     # connect to cytoxnet database
-    engine = create_engine('sqlite:///cytoxnet.db', echo = True)
+    engine = create_engine('sqlite:///cytoxnet.db', echo=True)
     engine.connect()
     meta = MetaData()
-    
+
     # create SQL table from dataframe
-    dataframe.to_sql(name=str(table_name), con=engine, if_exists='replace', index=False, dtype=header_info)
+    dataframe.to_sql(
+        name=str(table_name),
+        con=engine,
+        if_exists='replace',
+        index=False,
+        dtype=header_info)
 
 
-
-#def tables_to_database(dataframe_dict):
+# def tables_to_database(dataframe_dict):
     """
     Calls 'table_creater' function to generate multiple SQL tables at once from a dictionary
     with table names as the keys and dataframe objects as the values.  Such a dictionary is
@@ -67,66 +72,97 @@ def table_creator(table_name, dataframe):
     # call 'table_creater' function and iterate through dictionary
 
 
-
 def query_to_dataframe(tables, features_list=None):
     """
     Queryies selected tables within cytoxnet database, returning all columns from selected
-    tables as well as the smiles 
+    tables as well as the smiles
     """
     # connect to cytoxnet database
-    engine = create_engine('sqlite:///cytoxnet.db', echo = False)
+    engine = create_engine('sqlite:///cytoxnet.db', echo=False)
     meta = MetaData()
 
     # get list of tables to query
-    if type(tables) != list:
+    if not isinstance(tables, list):
         tables = [tables]
     else:
         pass
-    
-    tables_to_query = []
-    table_columns = []
-    
+
+    try:
+        len(tables) <= 2
+    except BaseException:
+        print('Can only pass a maximum of two lists at a time.')
+
+    table_object_list = []
+    table_column_list = []
+
     # load existing tables
     compounds = Table('compounds', meta, autoload=True, autoload_with=engine)
     for table in tables:
         table = Table(str(table), meta, autoload=True, autoload_with=engine)
-        
+
+        # place table objects inside list
+        table_object_list.append(table)
+
         # get list of column names and drop 'smiles' column
         column_list = []
-        
+
         for c in table.c:
-            if c.name !='smiles':
+            if c.name != 'smiles':
                 column_list.append(c.name)
             else:
                 continue
-            
-        for column_name in column_list:
-            table_columns.append(getattr(table.c, column_name).label(str(table)+'_'+column_name))
-            
-        # create statements to include in query for each table
-        tables_to_query.append(table.c.smiles == compounds.c.smiles)
-        
-    # get list of features to include
 
-    if features_list != None:    
-    
-        if type(features_list) != list:
+        for column_name in column_list:
+            table_column_list.append(
+                getattr(
+                    table.c,
+                    column_name).label(
+                    str(table) +
+                    '_' +
+                    column_name))
+
+    # get list of features to include
+    if features_list is not None:
+        if not isinstance(features_list, list):
             features_list = [features_list]
         else:
             pass
-    
-        select_stm = []
-    
+
+        features_select = []
+
         for feature in features_list:
-            select_stm.append(getattr(compounds.c, feature))
-        
-        # generate select statement
-        stmt = select([*table_columns, compounds.c.smiles, *select_stm]).where(or_(*tables_to_query)).distinct()
-                        
+            features_select.append(getattr(compounds.c, feature))
+
+        select_statement = [
+            *table_column_list,
+            compounds.c.smiles,
+            *features_select]
+
     else:
-        # generate select statment
-        stmt = select(*table_columns, compounds.c.smiles).where(or_(*tables_to_query)).distinct()
-    
+        select_statement = [*table_column_list, compounds.c.smiles]
+
+    # if more than one table being queried, perform a full-outer join
+    if len(table_object_list) > 1:
+        j_1 = table_object_list[0].join(
+            table_object_list[1],
+            table_object_list[0].c.smiles == table_object_list[1].c.smiles,
+            isouter=True)
+        j_2 = table_object_list[1].join(
+            table_object_list[0],
+            table_object_list[1].c.smiles == table_object_list[0].c.smiles,
+            isouter=True)
+        stmt_1 = select(select_statement).select_from(j_1).where(
+            compounds.c.smiles == table_object_list[0].c.smiles).distinct()
+        stmt_2 = select(select_statement).select_from(j_2).where(
+            and_(
+                compounds.c.smiles == table_object_list[1].c.smiles,
+                table_object_list[0].c.smiles is None))
+        stmt = union(*[stmt_1, stmt_2])
+
+    elif len(table_object_list) == 1:
+        stmt = select(select_statement).select_from(table_object_list[0].outerjoin(
+            compounds, table_object_list[0].c.smiles == compounds.c.smiles))
+
     conn = engine.connect()
     dataframe = pd.read_sql(sql=stmt, con=conn)
 

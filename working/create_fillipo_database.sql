f766ef94d7da5243a1a30bd1f9120e89dffc0644
Rory McGuire <roryjmcguire@gmail.com>
2021-06-06 19:47:11 -0700

    Removed several old csv and sql files

--- working/create_fillipo_database.sql
+++
@@ -1,9 +0,0 @@
-.headers on
-.mode csv
-.output fillipo_database.csv
-
-SELECT FILLIPO.id AS id, COMPOUNDS.id AS comp_id, FILLIPO.species AS species, FILLIPO.daphnia_ec50 AS daphnia_ec50, FILLIPO.fish_lc50 AS fish_lc50, FILLIPO.algea_ec50 AS algea_ec50, FILLIPO.units AS units, FILLIPO.source AS source
-FROM FILLIPO
-INNER JOIN COMPOUNDS ON COMPOUNDs.smiles = FILLIPO.smiles
-WHERE FILLIPO.id IS NOT NULL
-;
--- working/create_rld_database.sql
+++
@@ -1,9 +0,0 @@
-.headers on
-.mode csv
-.output RLD_database.csv
-
-SELECT RLD.id AS id, COMPOUNDS.id AS comp_id, RLD.compound_name AS compound_name, RLD.species AS species, RLD.ld50 AS ld50, RLD.units AS units, RLD.source AS source
-FROM RLD
-INNER JOIN COMPOUNDS ON COMPOUNDs.smiles = RLD.smiles
-WHERE RLD.id IS NOT NULL
-;
